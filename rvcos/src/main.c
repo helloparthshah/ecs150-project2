35d30e3a70b06b5f928644d0a48c3cab11247637
helloparthshah <helloparthshah@gmail.com>
2021-10-10 19:06:47 -0700

    Added cartridge status

--- rvcos/src/main.c
+++ rvcos/src/main.c
@@ -1,59 +1,72 @@
+// #include "RVCOS.h"
 #include <stdint.h>
 
 volatile int global = 42;
 volatile uint32_t controller_status = 0;
+volatile uint32_t cartridge_status = 0;
 
 volatile char *VIDEO_MEMORY = (volatile char *)(0x50000000 + 0xFE800);
+
+int len(char *p) {
+  int c = 0;
+  while (*p != '\0') {
+    c++;
+    *p++;
+  }
+  return (c);
+}
+
+void writec(char c, int pos) { VIDEO_MEMORY[pos] = c; }
+
+void write(char *c, int start) {
+  for (int i = start; i < start + len(c); i++) {
+    writec(c[i], i);
+  }
+}
+
 int main() {
-    int a = 4;
-    int b = 12;
-    int last_global = 42;
-    int x_pos = 12;
+  int a = 4;
+  int b = 12;
+  int last_global = 42;
+  int x_pos = 12;
 
-    VIDEO_MEMORY[0] = 'H';
-    VIDEO_MEMORY[1] = 'e';
-    VIDEO_MEMORY[2] = 'l';
-    VIDEO_MEMORY[3] = 'l';
-    VIDEO_MEMORY[4] = 'o';
-    VIDEO_MEMORY[5] = ' ';
-    VIDEO_MEMORY[6] = 'W';
-    VIDEO_MEMORY[7] = 'o';
-    VIDEO_MEMORY[8] = 'r';
-    VIDEO_MEMORY[9] = 'l';
-    VIDEO_MEMORY[10] = 'd';
-    VIDEO_MEMORY[11] = '!';
-    VIDEO_MEMORY[12] = 'X';
+  write("Hello World!X", 0);
 
+  while (1) {
+    int c = a + b + global;
+    if (global != last_global) {
+      if (cartridge_status)
+        writec(cartridge_status + '0', 0);
 
-    while (1) {
-        int c = a + b + global;
-        if(global != last_global){
-            if(controller_status){
-                VIDEO_MEMORY[x_pos] = ' ';
-                if(controller_status & 0x1){
-                    if(x_pos & 0x3F){
-                        x_pos--;
-                    }
-                }
-                if(controller_status & 0x2){
-                    if(x_pos >= 0x40){
-                        x_pos -= 0x40;
-                    }
-                }
-                if(controller_status & 0x4){
-                    if(x_pos < 0x8C0){
-                        x_pos += 0x40;
-                    }
-                }
-                if(controller_status & 0x8){
-                    if((x_pos & 0x3F) != 0x3F){
-                        x_pos++;
-                    }
-                }
-                VIDEO_MEMORY[x_pos] = 'X';
-            }
-            last_global = global;
+      if (controller_status) {
+        writec(controller_status + '0', 1);
+        VIDEO_MEMORY[x_pos] = ' ';
+        VIDEO_MEMORY[0] = ' ';
+        if (controller_status & 0x1) {
+          if (x_pos & 0x3F) {
+            x_pos--;
+          }
         }
+        if (controller_status & 0x2) {
+          if (x_pos >= 0x40) {
+            x_pos -= 0x40;
+          }
+        }
+        if (controller_status & 0x4) {
+          if (x_pos < 0x8C0) {
+            x_pos += 0x40;
+          }
+        }
+        if (controller_status & 0x8) {
+          if ((x_pos & 0x3F) != 0x3F) {
+            x_pos++;
+          }
+        }
+        writec('X', x_pos);
+        // VIDEO_MEMORY[x_pos] = 'X';
+      }
+      last_global = global;
     }
-    return 0;
+  }
+  return 0;
 }
--- rvcos/src/startup.c
+++ rvcos/src/startup.c
@@ -8,64 +8,67 @@ extern uint8_t _esdata[];
 extern uint8_t _bss[];
 extern uint8_t _ebss[];
 
-// Adapted from https://stackoverflow.com/questions/58947716/how-to-interact-with-risc-v-csrs-by-using-gcc-c-code
-__attribute__((always_inline)) inline uint32_t csr_mstatus_read(void){
-    uint32_t result;
-    asm volatile ("csrr %0, mstatus" : "=r"(result));
-    return result;
+// Adapted from
+// https://stackoverflow.com/questions/58947716/how-to-interact-with-risc-v-csrs-by-using-gcc-c-code
+__attribute__((always_inline)) inline uint32_t csr_mstatus_read(void) {
+  uint32_t result;
+  asm volatile("csrr %0, mstatus" : "=r"(result));
+  return result;
 }
 
-__attribute__((always_inline)) inline void csr_mstatus_write(uint32_t val){
-    asm volatile ("csrw mstatus, %0" : : "r"(val));
+__attribute__((always_inline)) inline void csr_mstatus_write(uint32_t val) {
+  asm volatile("csrw mstatus, %0" : : "r"(val));
 }
 
-__attribute__((always_inline)) inline void csr_write_mie(uint32_t val){
-    asm volatile ("csrw mie, %0" : : "r"(val));
+__attribute__((always_inline)) inline void csr_write_mie(uint32_t val) {
+  asm volatile("csrw mie, %0" : : "r"(val));
 }
 
-__attribute__((always_inline)) inline void csr_enable_interrupts(void){
-    asm volatile ("csrsi mstatus, 0x8");
+__attribute__((always_inline)) inline void csr_enable_interrupts(void) {
+  asm volatile("csrsi mstatus, 0x8");
 }
 
-__attribute__((always_inline)) inline void csr_disable_interrupts(void){
-    asm volatile ("csrci mstatus, 0x8");
+__attribute__((always_inline)) inline void csr_disable_interrupts(void) {
+  asm volatile("csrci mstatus, 0x8");
 }
 
-#define MTIME_LOW       (*((volatile uint32_t *)0x40000008))
-#define MTIME_HIGH      (*((volatile uint32_t *)0x4000000C))
-#define MTIMECMP_LOW    (*((volatile uint32_t *)0x40000010))
-#define MTIMECMP_HIGH   (*((volatile uint32_t *)0x40000014))
-#define CONTROLLER      (*((volatile uint32_t *)0x40000018))
+#define MTIME_LOW (*((volatile uint32_t *)0x40000008))
+#define MTIME_HIGH (*((volatile uint32_t *)0x4000000C))
+#define MTIMECMP_LOW (*((volatile uint32_t *)0x40000010))
+#define MTIMECMP_HIGH (*((volatile uint32_t *)0x40000014))
+#define CONTROLLER (*((volatile uint32_t *)0x40000018))
+#define CARTRIDGE (*((volatile uint32_t *)0x4000001C))
 
-void init(void){
-    uint8_t *Source = _erodata;
-    uint8_t *Base = _data < _sdata ? _data : _sdata;
-    uint8_t *End = _edata > _esdata ? _edata : _esdata;
+void init(void) {
+  uint8_t *Source = _erodata;
+  uint8_t *Base = _data < _sdata ? _data : _sdata;
+  uint8_t *End = _edata > _esdata ? _edata : _esdata;
 
-    while(Base < End){
-        *Base++ = *Source++;
-    }
-    Base = _bss;
-    End = _ebss;
-    while(Base < End){
-        *Base++ = 0;
-    }
+  while (Base < End) {
+    *Base++ = *Source++;
+  }
+  Base = _bss;
+  End = _ebss;
+  while (Base < End) {
+    *Base++ = 0;
+  }
 
-    csr_write_mie(0x888);       // Enable all interrupt soruces
-    csr_enable_interrupts();    // Global interrupt enable
-    MTIMECMP_LOW = 1;
-    MTIMECMP_HIGH = 0;
+  csr_write_mie(0x888);    // Enable all interrupt sources
+  csr_enable_interrupts(); // Global interrupt enable
+  MTIMECMP_LOW = 1;
+  MTIMECMP_HIGH = 0;
 }
 
 extern volatile int global;
 extern volatile uint32_t controller_status;
+extern volatile uint32_t cartridge_status;
 
-void c_interrupt_handler(void){
-    uint64_t NewCompare = (((uint64_t)MTIMECMP_HIGH)<<32) | MTIMECMP_LOW;
-    NewCompare += 100;
-    MTIMECMP_HIGH = NewCompare>>32;
-    MTIMECMP_LOW = NewCompare;
-    global++;
-    controller_status = CONTROLLER;
+void c_interrupt_handler(void) {
+  uint64_t NewCompare = (((uint64_t)MTIMECMP_HIGH) << 32) | MTIMECMP_LOW;
+  NewCompare += 100;
+  MTIMECMP_HIGH = NewCompare >> 32;
+  MTIMECMP_LOW = NewCompare;
+  global++;
+  controller_status = CONTROLLER;
+  cartridge_status = CARTRIDGE;
 }
-
